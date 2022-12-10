6b4ed15d6b2888a4d456c3fe268845fb4ed1a0c5
yasui <s.yasui@accelia.net>
2022-12-10 23:03:11 +0900

    ISSUE-#1: feature/add_collector libsの__init__を追加
    モジュールを参照するための定義ファイル追加

---
+++ trend-collector/trendcollector/libs/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env /Users/yasui/Practice/trend_collector/trend-collector/venv/bin/python
+
+from .client import *
+from .twitter_v2 import *
+from .conf import *
+from .response import *
