79e40834eec69ddd4a18d3666e8e02cfa0ba9432
yasui <s.yasui@accelia.net>
2022-12-11 21:42:57 +0900

    ISSUE-#1: feature/add_collector DBコネクション切断エラー用例外処理追加
    APIサーバ起動中にDBとのコネクションが切断された際の例外を補足する処理追加

--- trend-collector/trendcollector/server.py
+++ trend-collector/trendcollector/server.py
@@ -1,3 +1,7 @@
+import logging
+import sqlalchemy
+import pymysql
+
 from fastapi import FastAPI
 from libs.conf import Environment
 from libs.infrastractures.database import TwitterRepository
@@ -14,27 +18,51 @@ twitter_v2_cli = TwitterV2(
 )
 
 
+try:
+    twitter_db_cli = TwitterRepository(env.db_user, env.db_pass, env.db_name, env.host, env.port)
 
-twitter_db_cli = TwitterRepository(env.db_user, env.db_pass, env.db_name, env.host, env.port)
+    twitter_svc = TwitterCollector(twitter_db_cli, twitter_v2_cli)
 
-twitter_svc = TwitterCollector(twitter_db_cli, twitter_v2_cli)
-
-@app.get("/accounts",
-         response_model=AccountsReply,
-         )
-async def list_accounts():
-    resp = twitter_svc.list_accounts()
-    res = [Account(account_id=r.account_id, name=r.name, user_name=r.user_name) for r in resp]
-    return AccountsReply(result=res)
-
-
-@app.get("/accounts/me",
-         response_model=AccountReply,
-         responses={401: {"model": ErrorReply}, 500: {"model": ErrorReply}}
-         )
-async def root():
-    resp = twitter_v2_cli.get_me()
-    return AccountReply(result=Account(account_id=resp.account_id, name=resp.name, user_name=resp.user_name))
+    @app.get("/accounts",
+             response_model=AccountsReply,
+             )
+    async def list_accounts():
+        resp = twitter_svc.list_accounts()
+        res = [Account(account_id=r.account_id, name=r.name, user_name=r.user_name) for r in resp]
+        return AccountsReply(result=res)
 
 
-app.add_middleware(HttpErrorMiddleware)
+    @app.get("/accounts/me",
+             response_model=AccountReply,
+             responses={401: {"model": ErrorReply}, 500: {"model": ErrorReply}}
+             )
+    async def get_account():
+        resp = twitter_v2_cli.get_me()
+        return AccountReply(result=Account(account_id=resp.account_id, name=resp.name, user_name=resp.user_name))
+
+
+    @app.get("/accounts/{account_id}",
+             response_model=AccountReply,
+             responses={401: {"model": ErrorReply}, 500: {"model": ErrorReply}}
+             )
+    async def get_account(account_id: int):
+        resp = twitter_svc.get_account(account_id)
+        return AccountReply(result=Account(account_id=resp.account_id, name=resp.name, user_name=resp.user_name))
+
+
+    @app.put("/account/{id}",
+             response_model=AccountReply,
+             responses={401: {"model": ErrorReply}, 500: {"model": ErrorReply}}
+             )
+    async def update_account(account_id: int):
+        resp = twitter_svc.update_account(account_id)
+        return AccountReply(result=Account(account_id=resp.account_id, name=resp.name, user_name=resp.user_name))
+
+    app.add_middleware(HttpErrorMiddleware)
+
+# DBコネクションの切断などで切れた場合に捕捉するコード
+# TODO logging後にシャットダウンして通知しないと気づかない
+except sqlalchemy.exc.OperationalError as e:
+    logging.fatal(e)
+except pymysql.err.OperationalError as e:
+    logging.fatal(e)
