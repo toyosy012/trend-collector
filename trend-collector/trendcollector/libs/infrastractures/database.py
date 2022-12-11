edf99b72a641e4f723334d83be789b49b90188fd
yasui <s.yasui@accelia.net>
2022-12-11 21:26:24 +0900

    ISSUE-#1: feature/add_collector DBリポジトリのメソッドを実装
    DBアクセサーの抽象メソッドのリスト処理を実装
    
    アカウントリスト取得
    以下の抽象化を満たすために定義のみ実装
    自身のアカウント情報取得
    自身のアカウント情報更新
    アカウント情報取得
    アカウント情報更新

--- trend-collector/trendcollector/libs/infrastractures/database.py
+++ trend-collector/trendcollector/libs/infrastractures/database.py
@@ -1,7 +1,48 @@
+import sqlalchemy
+import sqlalchemy.pool as pool
+from sqlalchemy.orm import scoped_session, sessionmaker, Session
+from typing import List
+from .schemas import TwitterAccountTable
 from ..services.accessor import DBAccessor
 from ..models.twitter import TwitterAccount
 
 
-class TwitterDB(DBAccessor):
+# https://cloud.google.com/sql/docs/mysql/manage-connections?hl=ja
 
-    def update_me(self) -> TwitterAccount: pass
+
+class TwitterRepository(DBAccessor):
+    engine: sqlalchemy.engine.base.Engine
+    pool: sqlalchemy.pool.QueuePool
+
+    def __init__(self, db_user: str, db_pass: str, db_name: str, host: str, port: int):
+        self.engine = sqlalchemy.create_engine(
+            # postgresql+pg8000://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+            sqlalchemy.engine.url.URL.create(
+                drivername="mysql+pymysql",
+                username=db_user,
+                password=db_pass,
+                database=db_name,
+                host=host,
+                port=port,
+            )
+        )
+        self.pool = pool.QueuePool(self.engine.connect(), max_overflow=10, pool_size=5)
+        self.session_factory = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+    def get_me(self) -> TwitterAccount:
+        return TwitterAccount()
+
+    def update_me(self, twitter_account: TwitterAccount) -> TwitterAccount:
+        return TwitterAccount()
+
+    def list_accounts(self) -> List[TwitterAccount]:
+        session = scoped_session(self.session_factory)
+        rows = session.query(TwitterAccountTable)
+        accounts = [TwitterAccount(account_id=r.account_id, name=r.name, user_name=r.user_name) for r in rows]
+        return accounts
+
+    def update_account(self, account: TwitterAccount) -> TwitterAccount:
+        return TwitterAccount()
+
+    def get_account(self, account_id: int) -> TwitterAccount:
+        return TwitterAccount()
