c261e7b5a6ff53914defb3261c1a9f290635c3cb
yasui <s.yasui@accelia.net>
2022-12-11 21:41:12 +0900

    ISSUE-#1: feature/add_collector APIハンドラー追加
    Twitterアカウント用のハンドラ追加

--- trend-collector/trendcollector/server.py
+++ trend-collector/trendcollector/server.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from libs.conf import Environment
+from libs.infrastractures.database import TwitterRepository
+from libs.services.collector import TwitterCollector
 from libs.twitter_v2 import TwitterV2
-from libs.response import Account, ErrorReply, HttpErrorMiddleware, AccountReply
+from libs.response import Account, AccountReply, AccountsReply, ErrorReply, HttpErrorMiddleware
 
 env = Environment()
 app = FastAPI()
@@ -10,6 +12,20 @@ twitter_v2_cli = TwitterV2(
     env.consumer_key, env.consumer_secret,
     env.access_token, env.access_token_secret
 )
+
+
+
+twitter_db_cli = TwitterRepository(env.db_user, env.db_pass, env.db_name, env.host, env.port)
+
+twitter_svc = TwitterCollector(twitter_db_cli, twitter_v2_cli)
+
+@app.get("/accounts",
+         response_model=AccountsReply,
+         )
+async def list_accounts():
+    resp = twitter_svc.list_accounts()
+    res = [Account(account_id=r.account_id, name=r.name, user_name=r.user_name) for r in resp]
+    return AccountsReply(result=res)
 
 
 @app.get("/accounts/me",
