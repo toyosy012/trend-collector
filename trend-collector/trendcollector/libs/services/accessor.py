89061c96d22d2233245f34668046973b44c85629
yasui <s.yasui@accelia.net>
2022-12-11 21:21:41 +0900

    ISSUE-#1: feature/add_collector DBアクセサーへ抽象化メソッドを追加
    TwitterAccount情報のCRU処理を定義
    
    自身のアカウント情報取得
    自身のアカウント情報更新
    アカウントリスト取得
    アカウント情報取得
    アカウント情報更新

--- trend-collector/trendcollector/libs/services/accessor.py
+++ trend-collector/trendcollector/libs/services/accessor.py
@@ -1,8 +1,20 @@
 import abc
-
+from typing import List
 from ..models.twitter import TwitterAccount
 
 
 class DBAccessor(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def update_me(self) -> TwitterAccount: pass
+    def get_me(self) -> TwitterAccount: pass
+
+    @abc.abstractmethod
+    def update_me(self, account: TwitterAccount) -> TwitterAccount: pass
+
+    @abc.abstractmethod
+    def list_accounts(self) -> List[TwitterAccount]: pass
+
+    @abc.abstractmethod
+    def update_account(self, account: TwitterAccount) -> TwitterAccount: pass
+
+    @abc.abstractmethod
+    def get_account(self, account_id: int) -> TwitterAccount: pass
