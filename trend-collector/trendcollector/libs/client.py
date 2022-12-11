1c89e99960b79a5ea9e39677ab8755447cd607dc
yasui <s.yasui@accelia.net>
2022-12-11 10:41:04 +0900

    ISSUE-#1: feature/add_collector 実装からのレスポンス型を定義
    アカウント情報格納クラス定義
    
    基底例外型のメンバ変数を細かく再定義

--- trend-collector/trendcollector/libs/client.py
+++ trend-collector/trendcollector/libs/client.py
@@ -4,10 +4,22 @@ import abc
 class CustomException(Exception):
     def __init__(self, code: int, message: str, details: list[str]):
         self.code = code
-        self.details = {"message": message, "details": details}
+        self.message = message
+        self.details = details
+
+
+class TwitterAccount:
+    account_id: int
+    name: str
+    user_name: str
+
+    def __init__(self, account_id: int, name: str, user_name: str):
+        self.account_id = account_id
+        self.name = name
+        self.user_name = user_name
 
 
 class Twitter(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def get_me(self) -> dict: pass
+    def get_me(self) -> TwitterAccount: pass
 
