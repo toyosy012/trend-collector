bbb3187dbc83a5f0137f584e1ba2a95d667df415
yasui <s.yasui@accelia.net>
2022-12-11 10:46:17 +0900

    ISSUE-#1: feature/add_collector 自身のアカウント情報を取得するAPIを修正
    パスを適切に修正
    
    swaggerでAPIの仕様を取得できるように修正
    返却クラスを用いてリターンするように修正

--- trend-collector/trendcollector/server.py
+++ trend-collector/trendcollector/server.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
 from libs.conf import Environment
 from libs.twitter_v2 import TwitterV2
-from libs.response import HttpErrorMiddleware
-
+from libs.response import Account, ErrorReply, HttpErrorMiddleware, AccountReply
 
 env = Environment()
 app = FastAPI()
@@ -14,9 +12,13 @@ twitter_v2_cli = TwitterV2(
 )
 
 
-@app.get("/")
+@app.get("/accounts/me",
+         response_model=AccountReply,
+         responses={401: {"model": ErrorReply}, 500: {"model": ErrorReply}}
+         )
 async def root():
     resp = twitter_v2_cli.get_me()
-    return {"result": jsonable_encoder(resp)}
+    return AccountReply(result=Account(account_id=resp.account_id, name=resp.name, user_name=resp.user_name))
+
 
 app.add_middleware(HttpErrorMiddleware)
