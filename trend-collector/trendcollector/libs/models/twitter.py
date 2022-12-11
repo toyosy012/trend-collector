8ffb12a55b0c041b7717551d12798f68b453ac26
yasui <s.yasui@accelia.net>
2022-12-11 21:33:11 +0900

    ISSUE-#1: feature/add_collector Twitterアカウント型に初期化メソッド追加
    __init__メソッド追加

--- trend-collector/trendcollector/libs/models/twitter.py
+++ trend-collector/trendcollector/libs/models/twitter.py
@@ -2,3 +2,8 @@ class TwitterAccount:
     account_id: int
     name: str
     user_name: str
+
+    def __init__(self, account_id: int, name: str, user_name: str):
+        self.account_id = account_id
+        self.name = name
+        self.user_name = user_name
