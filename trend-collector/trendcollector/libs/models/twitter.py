fdaba7fc4e80cfff55f5664c24f9aa82146a8a2b
yasui <s.yasui@accelia.net>
2022-12-17 00:02:35 +0900

    ISSUE-#1: feature/add_collector Twitter関連モデル修正
    アカウント型にIDのメンバーを追加
    
    初期化処理の引数にidを追加

--- trend-collector/trendcollector/libs/models/twitter.py
+++ trend-collector/trendcollector/libs/models/twitter.py
@@ -1,9 +1,11 @@
 class TwitterAccount:
+    user_id: int
     account_id: int
     name: str
     user_name: str
 
-    def __init__(self, account_id: int, name: str, user_name: str):
+    def __init__(self, user_id: int, account_id: int, name: str, user_name: str):
+        self.user_id = user_id
         self.account_id = account_id
         self.name = name
         self.user_name = user_name
