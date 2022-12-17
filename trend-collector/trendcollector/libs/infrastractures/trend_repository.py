1b514e5d546eb82ccd7e22ee9d422c18ec982fb0
yasui <s.yasui@accelia.net>
2022-12-17 16:59:07 +0900

    ISSUE-#1: feature/add_collector トレンド情報のupsert処理追加
    追加更新処理追加
    
    存在しないトレンドは作成し、存在する場合はtweet_volumeを更新する処理を追加
    例外処理デコレータにDBへの不正リクエストハンドラを追加

--- trend-collector/trendcollector/libs/infrastractures/trend_repository.py
+++ trend-collector/trendcollector/libs/infrastractures/trend_repository.py
@@ -2,12 +2,15 @@ import functools
 
 from http import HTTPStatus
 from logging import Logger
+from sqlalchemy.dialects import mysql
 from sqlalchemy.exc import OperationalError as SQLAlchemyOperationalError
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.engine import Engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, Session
 
+from .schemas import TrendTable
 from ..models import Trend
-from ..services.accessor import TrendAccessor, OperationalException, FAILED_UPDATE_ACCOUNT
+from ..services.accessor import InvalidRequestException, TrendAccessor, OperationalException, FAILED_FETCH_TRENDS
 
 
 # コールバック関数の引数(*args, **kwargs)をCallableで表現することは不可能なので型ヒントは書かない
@@ -17,18 +20,20 @@ def handle_exception(func):
         try:
             return func(*args, **kwargs)
         except SQLAlchemyOperationalError as e:
-            raise OperationalException(HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_UPDATE_ACCOUNT, list(e.args))
+            raise OperationalException(HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_FETCH_TRENDS, list(e.args))
+        except InvalidRequestError as e:
+            raise InvalidRequestException(HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_FETCH_TRENDS, list(e.args))
 
     return _handler_wrapper
 
 
-class TwitterAccountRepository(TrendAccessor):
+class TrendRepository(TrendAccessor):
     engine: Engine
     logger: Logger
 
     def __init__(self, engine: Engine, logger: Logger):
         self.engine = engine
-        self.session = scoped_session(sessionmaker(autocommit=True, autoflush=True, bind=self.engine))
+        self.session_factory = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=self.engine))
         self.logger = logger
 
     @handle_exception
@@ -40,8 +45,29 @@ class TwitterAccountRepository(TrendAccessor):
         pass
 
     @handle_exception
-    def update(self, trend: Trend) -> Trend:
-        pass
+    def upsert(self, trends: [Trend]) -> [Trend]:
+        insert_stmt = mysql.insert(TrendTable).values([
+            dict(
+                name=t.name,
+                query=t.query,
+                tweet_volume=t.tweet_volume,
+            )
+            for t in trends
+        ])
+        upsert_stmt = insert_stmt.on_duplicate_key_update(
+            tweet_volume=insert_stmt.inserted.tweet_volume,
+        )
+
+        session: Session = self.session_factory()
+        try:
+            session.execute(upsert_stmt)
+            session.commit()
+            return True
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
 
     @handle_exception
     def delete(self, _id: int) -> bool:
