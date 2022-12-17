2f200a568fbeccdb3c25ceaa039ebb1145d40eab
yasui <s.yasui@accelia.net>
2022-12-17 11:47:49 +0900

    ISSUE-#1: feature/add_collector 例外を補足するデコレータの定義
    アクセサ実装メソッドの例外を統一的に処理するデコレータを定義
    
    各実装メソッドにデコレータを追加
    ラップする関数名を保持するようにfunctoolsを設定

--- trend-collector/trendcollector/libs/infrastractures/trend_repository.py
+++ trend-collector/trendcollector/libs/infrastractures/trend_repository.py
@@ -1,10 +1,25 @@
-from logging import Logger
+import functools
 
+from http import HTTPStatus
+from logging import Logger
+from sqlalchemy.exc import OperationalError as SQLAlchemyOperationalError
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from .. import Trend
-from ..services import TrendAccessor
+from ..models import Trend
+from ..services.accessor import TrendAccessor, OperationalException, FAILED_UPDATE_ACCOUNT
+
+
+# コールバック関数の引数(*args, **kwargs)をCallableで表現することは不可能なので型ヒントは書かない
+def handle_exception(func):
+    @functools.wraps(func)
+    def _handler_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except SQLAlchemyOperationalError as e:
+            raise OperationalException(HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_UPDATE_ACCOUNT, list(e.args))
+
+    return _handler_wrapper
 
 
 class TwitterAccountRepository(TrendAccessor):
@@ -16,15 +31,18 @@ class TwitterAccountRepository(TrendAccessor):
         self.session = scoped_session(sessionmaker(autocommit=True, autoflush=True, bind=self.engine))
         self.logger = logger
 
+    @handle_exception
     def get(self, _id: int) -> Trend:
         pass
 
+    @handle_exception
     def list(self) -> list[Trend]:
         pass
 
+    @handle_exception
     def update(self, trend: Trend) -> Trend:
         pass
 
+    @handle_exception
     def delete(self, _id: int) -> bool:
         pass
-
