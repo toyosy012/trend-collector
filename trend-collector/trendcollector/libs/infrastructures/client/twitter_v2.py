1e93354c90022f6333dffd55d65f22754012bff8
yasui <s.yasui@accelia.net>
2022-12-24 19:30:26 +0900

    ISSUE-#26: refactor/change_exception_handle Twitterクライアントの例外処理をメソッド毎に修正
    例外ハンドリングデコレータを削除
    
    twitter APIの認証・Forbidden例外処理を定義
    APIへのタイムアウト例外処理を定義

--- trend-collector/trendcollector/libs/infrastructures/client/twitter_v2.py
+++ trend-collector/trendcollector/libs/infrastructures/client/twitter_v2.py
@@ -1,5 +1,3 @@
-import functools
-import traceback
 from http import HTTPStatus
 from logging import Logger
 
@@ -8,44 +6,20 @@ from tweepy.client import Response
 
 from ...models import TwitterAccount, WoeidRawTrend
 from ...services import CustomException, client
-from ...services.custom_exception import IntervalServerException
+from ...services.custom_exception import Timeout, FETCH_ERROR
 
-FORBIDDEN_ACCESS = "アクセス権限がないために失敗"
-FAILED_GET_MY_ACCOUNT = "自身のアカウントの取得に失敗"
-FAILED_GET_TRENDS = "トレンドリストの取得に失敗"
-UNEXPECTED_ERROR = "予期せぬエラーが発生"
-TIMEOUT_REQUEST = "リクエストタイムアウト"
+FAILED_FETCH_ACCOUNT = "自身のアカウントの取得に失敗"
+FAILED_FETCH_TRENDS = "トレンドリストの取得に失敗"
 
 
 class TwitterForbidden(CustomException):
-    def __init__(self, code: int, message: str, details: list[str], stack_trace: str | None):
-        super().__init__(code, message, details, stack_trace)
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, message, details)
 
 
 class TwitterUnAuthorized(CustomException):
-    def __init__(self, code: int, message: str, details: list[str], stack_trace: str | None):
-        super().__init__(code, message, details, stack_trace)
-
-
-def handle_exception(func):
-    @functools.wraps(func)
-    def _handler_wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except tweepy.Unauthorized as e:
-            raise TwitterUnAuthorized(
-                HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_GET_MY_ACCOUNT, e.api_messages, traceback.format_exc())
-        except tweepy.errors.Forbidden as e:
-            raise TwitterForbidden(
-                HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_GET_TRENDS, e.api_messages, traceback.format_exc())
-        except TimeoutError as e:
-            raise IntervalServerException(
-                HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_GET_MY_ACCOUNT, list(e.args), traceback.format_exc())
-        except Exception as e:
-            raise IntervalServerException(
-                HTTPStatus.INTERNAL_SERVER_ERROR, FAILED_GET_MY_ACCOUNT, list(e.args), traceback.format_exc())
-
-    return _handler_wrapper
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, message, details)
 
 
 class TwitterV2(client.Twitter):
@@ -67,26 +41,44 @@ class TwitterV2(client.Twitter):
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
-    @handle_exception
     def get_me(self) -> TwitterAccount:
         try:
             resp = self.client.get_me()
             return TwitterAccount(0, resp[0]["id"], resp[0]["name"], resp[0]["username"])
-        except Exception as e:
-            raise e
+        except tweepy.Unauthorized as e:
+            raise TwitterUnAuthorized(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_ACCOUNT}", e.api_messages)
+        except tweepy.errors.Forbidden as e:
+            raise TwitterForbidden(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_ACCOUNT}", e.api_messages)
+        except TimeoutError as e:
+            raise Timeout(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_ACCOUNT}", list(e.args))
 
-    @handle_exception
     def get_account(self, _id: int, account_id: int) -> TwitterAccount:
         try:
             resp: Response = self.client.get_user(id=account_id)
             return TwitterAccount(_id, resp.data["id"], resp.data["name"], resp.data["username"])
-        except Exception as e:
-            raise e
+        except tweepy.Unauthorized as e:
+            raise TwitterUnAuthorized(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_ACCOUNT}", e.api_messages)
+        except tweepy.errors.Forbidden as e:
+            raise TwitterForbidden(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_ACCOUNT}", e.api_messages)
+        except TimeoutError as e:
+            raise Timeout(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_ACCOUNT}", list(e.args))
 
-    @handle_exception
     def list_trends(self, woeid: int) -> list[WoeidRawTrend]:
         try:
             resp = self.api.get_place_trends(woeid)[0]['trends']
             return [WoeidRawTrend(name=t["name"], query=t["query"], tweet_volume=t["tweet_volume"]) for t in resp]
-        except Exception as e:
-            raise e
+        except tweepy.Unauthorized as e:
+            raise TwitterUnAuthorized(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_TRENDS}", e.api_messages)
+        except tweepy.errors.Forbidden as e:
+            raise TwitterForbidden(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_TRENDS}", e.api_messages)
+        except TimeoutError as e:
+            raise Timeout(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"{FETCH_ERROR}: {FAILED_FETCH_TRENDS}", list(e.args))
