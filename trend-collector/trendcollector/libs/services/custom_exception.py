5d60c97d2265ba522a12a02a21dd4ea4ac943d1b
yasui <s.yasui@accelia.net>
2022-12-24 18:41:42 +0900

    ISSUE-#26: refactor/change_exception_handle 例外クラスの定義
    例外ハンドリングに用いるクラスを新しく定義
    
    InternalServerErrorを扱う規定例外クラスを定義
    タイムアウト例外クラスを定義
    DB例外の規定クラスを定義
    DBコネクション切断を表すクラスを定義
    テーブル毎のレコード取得例外クラスを定義
    ロギング済みの例外を扱うクラスを定義

--- trend-collector/trendcollector/libs/services/custom_exception.py
+++ trend-collector/trendcollector/libs/services/custom_exception.py
@@ -1,11 +1,48 @@
+DELETE_ERROR = "削除エラー"
+FETCH_ERROR = "取得エラー"
+SEARCH_ERROR = "検索エラー"
+UPDATE_ERROR = "更新エラー"
+
+
 class CustomException(Exception):
-    def __init__(self, code: int, message: str, details: list[str], stack_trace: str | None):
+    def __init__(self, code: int, message: str, details: list[str]):
         self.code = code
         self.message = message
         self.details = details
-        self.stack_trace = stack_trace
 
 
 class IntervalServerException(CustomException):
-    def __init__(self, code: int, message: str, details: list[str], stack_trace: str | None):
-        super().__init__(code, message, details, stack_trace)
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, message, details)
+
+
+class Timeout(IntervalServerException):
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, message, details)
+
+
+class DataBaseException(CustomException):
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, message, details)
+
+
+class DisconnectionDB(DataBaseException):
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, message, details)
+
+
+class NoTrendRecord(DataBaseException):
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, f"{message}: トレンドデータは存在しません", details)
+
+
+class NoTwitterAccountRecord(DataBaseException):
+    def __init__(self, code: int, message: str, details: list[str]):
+        super().__init__(code, f"{message}: Twitterアカウントデータは存在しません", details)
+
+
+class LoggedException(Exception):
+    def __init__(self, code: int, message: str, request_id: str):
+        self.code = code
+        self.message = message
+        self.request_id = request_id
