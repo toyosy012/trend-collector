77f5e9acddfac8d40689924abaa6934c4d14c56f
toyosy012 <toyosy012@gmail.com>
2022-12-30 23:55:38 +0900

    ISSUE-#39: feature/add_trend_metrics_api トレンドメトリクス取得API追加
    トレンドメトリクス用レスポンスモデルクラス追加
    
    メトリクスデータを保持するメンバ変数定義
    メトリクスデータ長のメンバ変数定義
    メトリクス用レスポンスモデルクラス追加
    レスポンスモデル用定数定義

--- trend-collector/trendcollector/libs/infrastructures/conf.py
+++ trend-collector/trendcollector/libs/infrastructures/conf.py
@@ -1,10 +1,16 @@
 from pydantic import BaseSettings, validator
 
+RECORD_ID = 1
 TWITTER_ACCOUNT_ID = 1000000000000000
 DISPLAY_NAME = "toyosy012"
 USER_NAME = "toyosy012"
 TREND_NAME = "ばあちゃん卒業"
+TREND_VOLUME = 1
 TREND_QUERY = "%E3%81%B0%E3%81%82%E3%81%A1%E3%82%83%E3%82%93%E5%8D%92%E6%A5%AD"
+INPUT_DATETIME = "2023-01-01T00:00:00"
+START_DATETIME = "2023-01-01T00:00:00"
+END_DATETIME = "2023-01-01T01:00:00"
+INPUT_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
 validate_error_format: str = "Validate error: {}"
 
