cc4ad0ae1a1d8153a1dfa3180fd6877b29819bbb
toyosy012 <toyosy012@gmail.com>
2023-01-03 23:23:59 +0900

    ISSUE-#97: refactor/change_collector_module コレクタモジュールの修正
    lintエラーを解決
    
    importのソート
    1行で定義していた抽象メソッドを複数行に修正
    長くなったメソッドの引数定義を複数行に修正

--- trend-collector/trendcollector/libs/services/collector.py
+++ trend-collector/trendcollector/libs/services/collector.py
@@ -1,31 +1,40 @@
 import abc
-
 from datetime import datetime
-from injector import inject, singleton
 from typing import Union
 
-from ..models import TrendMetrics, TrendSummary, TrendQuery, InputRawTrend
+from injector import inject, singleton
+
+from ..models import InputRawTrend, TrendMetrics, TrendQuery, TrendSummary
 from ..services.client import Twitter
 from .accessor import TrendAccessor
 
 
 class MediaCollectorSvc(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def get_trend(self, _id: int) -> TrendSummary: pass
+    def get_trend(self, _id: int) -> TrendSummary:
+        pass
 
     @abc.abstractmethod
-    def list_trends(self, page: int, counts: int) -> list[TrendSummary]: pass
+    def list_trends(self, page: int, counts: int) -> list[TrendSummary]:
+        pass
 
     @abc.abstractmethod
     def list_trend_metrics(
-            self, trend_id: int, start_time: datetime, end_time: datetime, granularity: Union[str | None]
-    ) -> TrendMetrics: pass
+            self,
+            trend_id: int,
+            start_time: datetime,
+            end_time: datetime,
+            granularity: Union[str | None]
+    ) -> TrendMetrics:
+        pass
 
     @abc.abstractmethod
-    def insert_trends(self, woeid: int) -> bool: pass
+    def insert_trends(self, woeid: int) -> bool:
+        pass
 
     @abc.abstractmethod
-    def delete_trend(self, trend: int) -> bool: pass
+    def delete_trend(self, trend: int) -> bool:
+        pass
 
 
 @singleton
@@ -42,8 +51,13 @@ class TwitterCollector(MediaCollectorSvc):
     def list_trends(self, page: int, counts: int) -> list[TrendSummary]:
         return self.trend_repo.list(page, counts)
 
-    def list_trend_metrics(self, trend_id: int,
-                           start_time: datetime, end_time: datetime, granularity: str) -> TrendMetrics:
+    def list_trend_metrics(
+            self,
+            trend_id: int,
+            start_time: datetime,
+            end_time: datetime,
+            granularity: str
+    ) -> TrendMetrics:
         trend = self.trend_repo.get(trend_id)
         query = TrendQuery(trend_id, trend.name)
 
