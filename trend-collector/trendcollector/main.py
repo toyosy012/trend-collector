a6d639fdcf1e9a38b26503984f8216a165736499
toyosy012 <toyosy012@gmail.com>
2023-01-04 04:11:41 +0900

    ISSUE-#111: refactor/sort_imports ポートをデフォルト値に戻す
    不要な変更なので修正

--- trend-collector/trendcollector/main.py
+++ trend-collector/trendcollector/main.py
@@ -90,4 +90,4 @@ app.add_middleware(HttpLoggingHandler)
 app.add_middleware(HttpErrorMiddleware)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
